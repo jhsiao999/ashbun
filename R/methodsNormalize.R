774e953b02b64a25be800c6874e9fee10628cb6d
jhsiao999 <joyce.hsiao1@gmail.com>
2017-10-02 15:36:28 -0500

    add package dependency

--- R/methodsNormalize.R
+++ R/methodsNormalize.R
@@ -76,7 +76,7 @@ normalize.rle <- function(counts, control = list(NULL)) {
   assertthat::assert_that(is.matrix(counts))
 
   #<--------------------------------------
-  libsize_factors <- estimateSizeFactorsForMatrix(counts)
+  libsize_factors <- DESeq2::estimateSizeFactorsForMatrix(counts)
 
   return(list(libsize_factors = libsize_factors))
 }
